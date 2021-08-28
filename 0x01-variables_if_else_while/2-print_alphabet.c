5dc631fe42754921adaee358340f5a14a3e5ac49
CodeMissy <milleniallblizz@gmail.com>
2021-08-28 13:56:46 -0700

    2-print_alphabet.c updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 'A' ; ch < = 'z' ; ch++)
+putchar(ch);
+
+return (0);
+}
