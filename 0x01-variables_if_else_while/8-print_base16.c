91f24121281e68469fe1bb54e3f3f022803ceb54
CodeMissy <milleniallblizz@gmail.com>
2021-08-29 14:27:38 -0700

    8-print_base16.c updated

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all single hexadecimal digits in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+int i = 48;
+for(i = 48; i < 58; i++)
+putchar(i);
+for (i = 'a'; i <= 'f'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+} 
