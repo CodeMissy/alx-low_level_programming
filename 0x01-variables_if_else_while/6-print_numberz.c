c824c7badfdfdf2c7ed60650f853e6692cee2d96
CodeMissy <milleniallblizz@gmail.com>
2021-08-29 13:00:25 -0700

    6-print_numberz.c updated

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all single digit decimal number
+ * Return: 0
+ */
+
+int main (void)
+{
+int Digit;
+int i = 48;
+
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+} 
+  
