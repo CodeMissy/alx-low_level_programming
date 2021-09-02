6aaee5ea18e51fc389d88d430ee4ce0f1a3913cc
CodeMissy <milleniallblizz@gmail.com>
2021-09-02 10:44:13 -0700

    2-mul.c updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies and returns sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,10 @@ int _putchar(char c);
 /* checks if letter is uppercase */
 int _isupper(int c);
 
-/* checks for digit */
+/* checks if an integer is a digit */
 int _isdigit(int c);
+
+/* performs multiplication of two integers */
+int mul(int a, int b);
 
 #endif
