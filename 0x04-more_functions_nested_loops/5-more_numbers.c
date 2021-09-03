d49e41b30fec39562bbed7d3e304caee3161347d
CodeMissy <milleniallblizz@gmail.com>
2021-09-03 02:19:19 -0700

    5-more_numbers.c updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,21 +2,18 @@
 
 /**
  * more_numbers - prints numbers 0-14 ten times
- *
- *
- * Return: void
  */
 
 void more_numbers(void)
 {
 int i, j;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 15; j++)
 {
 if (j >= 10)
 _putchar(j / 10 + 48);
-_putchar(i % 10 + 48);
+_putchar(j % 10 + 48);
 }
 _putchar('\n');
 }
