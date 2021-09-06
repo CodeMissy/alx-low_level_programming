d96c3e4e1624cc534414360722f3c33dfdc01bed
CodeMissy <milleniallblizz@gmail.com>
2021-09-06 08:16:40 -0700

    1-swap.c updated

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void swap_int - swaps the value of two integers
+ * @a: first integer used
+ * @b: second integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+swap_int(&a, &b);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,7 @@ int _putchar(char c);
 /* takes a pointer to int and updates its value */
 void reset_to_98(int *n);
 
+/* swaps the value of two integers */
+void swap_int(int *a, int *b);
+
 #endif
