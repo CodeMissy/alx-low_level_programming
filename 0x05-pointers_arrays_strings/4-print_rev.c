c8922936e0df48e694457fedcc00aca494bc2516
CodeMissy <milleniallblizz@gmail.com>
2021-09-06 11:25:11 -0700

    4-print_rev.c updated

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+₦include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @str: string to be printed
+ * Return: reversed string
+ */
+
+void print_rev(char *s)
+{
+int count = 0;
+for (str >= 1; str >= count; str++)
+_putchar(str);
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,7 @@ int _strlen(char *s);
 /* prints a string to stdout */
 void _puts(char *str);
 
+/* prints a string in reverse */
+void print_rev(char *s);
+
 #endif
