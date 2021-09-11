841786e5df148c28fe7ae477d79b9716a2ad8f29
CodeMissy <milleniallblizz@gmail.com>
2021-09-11 09:54:25 -0700

    2-strncpy.c updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,13 +5,13 @@
  * @src: string to copy from
  * @dest: destination to copy to
  * @n: number of bytes used while copying
- * Return: copied string to dest
+ * Return: pointer to dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n; && src[i] != '\0'; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
 for ( ; i < n; i++)
 dest[i] = '\0';
