bf6ae365ed030403c30dfc923005ab8ff10dfd52
CodeMissy <milleniallblizz@gmail.com>
2021-09-11 10:27:36 -0700

    3-strcmp.c updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-if ( s1 == s2)
+while (*s1 == *s2)
 {
-while (i < s1)
+if (*s1 == '\0')
 {
-if (s1[i] == s2[i])
-i++;
-else break;
+return (0);
 }
-return (diff);
+s1++;
+s2++;
 }
+return (*s1 - *s2);
 }
