488670fb15f3e4120d28c210e1b7d77fb29cc804
CodeMissy <milleniallblizz@gmail.com>
2021-09-11 08:05:21 -0700

    1-strcat.c updated

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,18 +2,21 @@
 
 /**
  * _strncat - contactenates two strings
- * @dest: string 1
- * @dest: string 2
+ * @dest: string to be contacenated to
+ * @src: string to be contacenated
  * @n: integer used
- * Return: void
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-char *dest, *src;
-int n;
-_strcpy(str1);
-_strcpy(str2);
-_strncat(str1, str2, sizeof(str1) (strlen(str1) - 1);
-_putchar(str1);
+int i;
+int n = 0;
+while (dest[n] != '\0')
+{
+dest++;
+}
+if (i = 0; i < n; i++)
+dest[n + i];
+return (dest);
 }
