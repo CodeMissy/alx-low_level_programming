025baf47d30d20df148826fbab39a4140fad30b0
CodeMissy <milleniallblizz@gmail.com>
2021-09-12 10:26:04 -0700

    5-string_toupper.c updated

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _toupper - changes all lowercase letters of a string to uppercase
+ * Return: uppercase letters
+ */
+
+char *string_toupper(char *)
+{
+str[i];
+/* this line continues to print characters until it reaches null */
+for (i = 0; str[i] != '\0'; i++)
+{
+/* if present character is lowercase, convert it to uppercase, 32 is used here because 32 is the difference between a and A, so subtracting 32 does the conversion */
+if (str[i] >= 'a'; && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+}
