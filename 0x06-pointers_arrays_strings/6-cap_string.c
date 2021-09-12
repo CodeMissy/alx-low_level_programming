decc62faace9d6a417d66d01d91bb0102b2d79cd
CodeMissy <milleniallblizz@gmail.com>
2021-09-12 13:26:08 -0700

    6-cap_string.c updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,40 +3,38 @@
 /**
  * cap_string - capitalizes first character of
  * every words of a string
- * @s: string
+ * @str: string
  * Return: return string
  */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 int i;
 /* capitalize first character of words */
 for (i = 0; str[i] != '\0'; i++)
 {
 /* check first character is lowercase alphabet */
-if (i == 0)
-{
 if ((str[i] >= 'a' && str[i] <= 'z'))
-str[i] = str[i] - 32; /* subtract 32 to make it capital */
+str[i] = str[i] - 32; /* - 32 to make it capital */
 continue; /* continue to the loop */
 }
-if (str[i] == ' ') /* check space */
+if
+(str[i] == ' ') /* check space */
+(str[i] == '\t')
+(str[i] == '\n')
+(str[i] == ',')
+(str[i] == ';')
+(str[i == '.')
+(str[i] == '!')
+(str[i] == '?')
+(str[i] == '"')
+(str[i] == '(')
+(str[i] == ')')
+(str[i] == '{')
+(str[i] == '}')
 {
-/* if space is found, check next character */
+/* if quotations is found, check next character */
 ++i;
-/* check next character is lowercase alphabet */
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32; /* subtract 32 to make it capital */
-continue; /* continue to the loop */
-}
-}
-else
-{
-/* all other uppercase characters should be in lowercase */
-if (str[i] >= 'A' && str[i] <= 'Z')
-str[i] = str[i] + 32; /* adds 32 to make it lowercase */
-}
 }
 return (s);
 }
