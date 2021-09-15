45b08d226e4570a73e3f2f94c56436758d57d6ba
CodeMissy <milleniallblizz@gmail.com>
2021-09-15 16:00:02 -0700

    0-puts_recursion updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,14 @@
 /**
  * _puts_recursion - prints a string
  * @s: character used
+ * @n: number of times string is printed
  * Return: void
  */
 
 _puts_recursion(char *s)
 {
-char i, int n;
-s = &i
-
-if (i <= n)
+int n;
+if (*s <= n)
 return(n);
 _putchar('\n');
 }
