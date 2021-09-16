a1151c2b8bb5f381f16d32b39dcb6e648a6e931e
CodeMissy <milleniallblizz@gmail.com>
2021-09-16 03:17:59 -0700

    3-factorial.c updated

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of a given number
+ * @n: number of strings
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+if (n == -1)
+{
+return (-1);
+}
+else
+return (factorial = n * (n-1));
+}
