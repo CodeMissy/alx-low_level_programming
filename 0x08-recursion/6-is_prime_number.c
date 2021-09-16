b33ded7ad0b6975dc54eda024c77b25d62312748
CodeMissy <milleniallblizz@gmail.com>
2021-09-16 11:36:01 -0700

    6-is_prime_number.c updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 /**
  * is_prime_number - checks if num is prime number
  * @n: num
- * Return: void
+ * Return: 1 if n is a prime number, 0 if n is not a prime number
  */
 
 int is_prime_number(int n)
