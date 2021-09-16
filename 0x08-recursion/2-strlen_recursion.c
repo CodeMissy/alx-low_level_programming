28f7220e6cada9c6c4deb5eb0d558baad11c3a58
CodeMissy <milleniallblizz@gmail.com>
2021-09-15 18:25:25 -0700

    _strlen_recursion updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,15 @@
 /**
  * _strlen_recursion - prints the length of string
  * @s: string
- * Return: void
+ * Return: string length
  */
 
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return (_strlen);
+return (0);
 }
 else
-*s++;
+return (1 + _strlen_recursion(s + 1));
 }
