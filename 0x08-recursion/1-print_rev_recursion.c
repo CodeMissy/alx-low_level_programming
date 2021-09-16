991ba1a25fc813d09abdb9fa44f7226fdea9876a
CodeMissy <milleniallblizz@gmail.com>
2021-09-15 17:44:47 -0700

    1-print_rev_recursion.c updated

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverses string
+ * @s: string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(*s);
+_print_rev_recursion(s +1);
+}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,23 @@ int _putchar(char c);
 /* prints a string */
 void _puts_recursion(char *s);
 
+/* prints a string in reverse */
+void _print_rev_recursion(char *s);
+
+/* returns the length of a string */
+int _strlen_recursion(char *s);
+
+/* returns the factorial of a given number */
+int factorial(int n);
+
+/* returns value of x raised to power y */
+int _pow_recursion(int x, int y);
+
+/* returns natural square root of a number */
+int _sqrt_recursion(int n);
+
+/* returns 1 if input is prime number */
+int is_prime_number(int n);
+
+
 #endif
