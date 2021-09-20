2e36ce87eb7ca80214d3ee556cfa1603af37f28c
CodeMissy <milleniallblizz@gmail.com>
2021-09-19 18:05:05 -0700

    static library created

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks if alphabet is uppercase
+ * @c: the integer to check
+ * Return: 1 if true, 0 if false.
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
