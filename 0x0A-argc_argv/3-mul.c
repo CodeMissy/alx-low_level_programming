9008322c85a9198d5a4fcf9aec6722d11064eb34
CodeMissy <milleniallblizz@gmail.com>
2021-09-21 16:25:13 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-int a, b, mul;
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-mul = a * b;
-if (argc <= 2)
+if (argc < 2 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
-
-printf("%d\n", mul);
+else
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
+}
 }
