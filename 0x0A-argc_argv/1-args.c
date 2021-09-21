240442ea58acf1196c223a16a427773577fd807a
CodeMissy <milleniallblizz@gmail.com>
2021-09-21 03:05:14 -0700

    1-args.c updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: counts number of arguments
+ * @argv: array storing arguments used in program
+ * Return: 0
+ */
+
+int main(int argc, char *argv __attribute__((unused)))
+{
+printf("%d\n", argc-i);
+return (0);
+}
