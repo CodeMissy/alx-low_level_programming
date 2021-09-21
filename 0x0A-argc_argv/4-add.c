1687b8787edf8d396cfa9d5f96af11b94aa30711
CodeMissy <milleniallblizz@gmail.com>
2021-09-21 16:56:24 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints addition of positive numbers
+ * @argc: argument counter
+ * @argv: argument storage
+ * Return: 0 but 1 if there are no digits
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc % 2 = 0)
+{
+printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
