b5db628caed735eb486da737e0e278591cc0fef3
CodeMissy <milleniallblizz@gmail.com>
2021-09-21 10:39:48 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,15 @@
 
 int main(int argc __argument__((unused)), char *argv[])
 {
-if (argv[1] && argv[2])
+int a, b, mul;
+if (argv <= 2)
 {
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-return (0);
-}
-else printf("Error\n");
+printf("Error\n");
 return (1);
+}
+a = atoi(argv[1]);
+b= atoi(argv[2]);
+mul = a * b;
+printf("%d\n", mul);
+return (0);
 }
