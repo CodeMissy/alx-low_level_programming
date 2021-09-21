306ebababc0313cb8ba710322b6052244321dac2
CodeMissy <milleniallblizz@gmail.com>
2021-09-21 03:46:16 -0700

    1-args.c updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-printf("%d\n", argc-i);
+(void) argv;
+printf("%d\n", argc - i);
 return (0);
 }
