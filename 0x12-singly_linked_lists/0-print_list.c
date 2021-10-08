54d800103f253cd753bd183bdb6eaf0e79af41d6
CodeMissy <milleniallblizz@gmail.com>
2021-10-08 08:53:08 -0700

    0-print_list.c updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,16 @@ size_t print_list(const list_t *h)
 {
 size_t count;
 count = 0;
-  
-h = temp = newnode;
-if (!h)
-printf("%d, %s", [0] (nil));
 
-while (h != 0)
-printf("%d", temp->data);
-temp = temp->next;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
 count++;
+}
+return (count);
 }
