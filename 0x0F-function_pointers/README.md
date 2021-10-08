6af2b327b0b1224e04782ccf73614447c8068b24
CodeMissy <milleniallblizz@gmail.com>
2021-10-08 02:22:47 -0700

    0-print_list.c

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,10 @@
+Function pointers are a special type of pointers that points to a function.
+Function pointers contains the codes of the function it is pointing to.
+
+Syntax for function pointers looks like thi:
+
+datatype (*ptr)(functions)
+
+eg.
+
+int (*ptr)(int a, int b)
---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_list - prints all singly list element
+ * list_t - singly list
+ * @h: head of list
+ * Return: void
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+const list_t *temp = h;
+
+while (temp != NULL)
+{
+if (temp->next == NULL)
+printf("%d", temp->value);
+}
+else
+{
+printf("%d, ", temp->value);
+}
+temp = temp->next;
+}
