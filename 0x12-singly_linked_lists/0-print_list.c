181871b9426d6640116ef86c7de7d897bcef93a5
CodeMissy <milleniallblizz@gmail.com>
2021-10-08 08:23:59 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,18 @@
  * Return: number of nodes
  */
 
+
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+size_t count;
+count = 0;
+  
+h = temp = newnode;
+if (!h)
+printf("%d, %s", [0] (nil));
 
-while (h)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
-
-else
-printf("[%d] %s\n", h->len, h->str);
-
-nodes++;
-h = h->next;
-}
-
-return (nodes);
+while (h != 0)
+printf("%d", temp->data);
+temp = temp->next;
+count++;
 }
